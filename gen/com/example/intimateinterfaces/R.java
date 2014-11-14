777f9f00ea4b981b7b6e0c511ac61732445ac13f
kott <ott.kristian@gmail.com>
2014-11-14 12:45:18 +0100

    Added line drawing to main activity

--- gen/com/example/intimateinterfaces/R.java
+++ gen/com/example/intimateinterfaces/R.java
@@ -33,10 +33,11 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_example=0x7f090005;
-        public static final int action_settings=0x7f090004;
+        public static final int action_example=0x7f090006;
+        public static final int action_settings=0x7f090005;
         public static final int container=0x7f090001;
         public static final int drawer_layout=0x7f090000;
+        public static final int drawingview=0x7f090004;
         public static final int navigation_drawer=0x7f090002;
         public static final int section_label=0x7f090003;
     }
---
+++ src/com/example/intimateinterfaces/DrawingView.java
@@ -0,0 +1,146 @@
+package com.example.intimateinterfaces;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawingView extends View{
+
+    public static final float TOUCH_TOLERANCE = 4;
+    public static final float TOUCH_STROKE_WIDTH = 15;
+
+    /**
+     * Drawing objects
+     */
+    protected Path mPath;
+    protected Paint mPaint;
+    protected Paint mPaintFinal;
+    protected Bitmap mBitmap;
+    protected Canvas mCanvas;
+
+    /**
+     * Indicates if you are drawing
+     */
+    protected boolean isDrawing = false;
+
+    /**
+     * Indicates if the drawing is ended
+     */
+    protected boolean isDrawingEnded = false;
+
+
+    protected float mStartX;
+    protected float mStartY;
+
+    protected float mx;
+    protected float my;
+
+    public DrawingView(Context context) {
+        super(context);
+        init();
+    }
+
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public DrawingView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(mBitmap, 0, 0, mPaint);
+    }
+
+
+    protected void init() {
+        mPath = new Path();
+
+        mPaint = new Paint(Paint.DITHER_FLAG);
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(getContext().getResources().getColor(android.R.color.holo_blue_dark));
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(TOUCH_STROKE_WIDTH);
+
+
+        mPaintFinal = new Paint(Paint.DITHER_FLAG);
+        mPaintFinal.setAntiAlias(true);
+        mPaintFinal.setDither(true);
+        mPaintFinal.setColor(getContext().getResources().getColor(android.R.color.holo_orange_dark));
+        mPaintFinal.setStyle(Paint.Style.STROKE);
+        mPaintFinal.setStrokeJoin(Paint.Join.ROUND);
+        mPaintFinal.setStrokeCap(Paint.Cap.ROUND);
+        mPaintFinal.setStrokeWidth(TOUCH_STROKE_WIDTH);
+    }
+
+    protected void reset() {
+        mPath = new Path();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mx = event.getX();
+        my = event.getY();
+        onTouchEventLine(event);
+        return true;
+    }
+
+    /**
+     * Draw the line on a touch event
+     * @param event
+     */
+    private void onTouchEventLine(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                isDrawing = true;
+                mStartX = mx;
+                mStartY = my;
+
+                mPath.reset();
+                mPath.moveTo(mx, my);
+
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+
+                float dx = Math.abs(mx - mStartX);
+                float dy = Math.abs(my - mStartY);
+                if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+                    mPath.quadTo(mStartX, mStartY, (mx + mStartX) / 2, (my + mStartY) / 2);
+                    mStartX = mx;
+                    mStartY = my;
+                }
+                mCanvas.drawPath(mPath, mPaint);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                isDrawing = false;
+                mPath.lineTo(mStartX, mStartY);
+                mCanvas.drawPath(mPath, mPaintFinal);
+                mPath.reset();
+                invalidate();
+                break;
+        }
+    }
+
+}
